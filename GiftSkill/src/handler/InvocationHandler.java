28ce6238cfe21d743bc194e5c1358fd4b8cf8491
Nick Lee <nichll@amazon.com>
2018-07-26 12:51:15 -0700

    Added code for alexa handler

--- GiftSkill/src/handler/InvocationHandler.java
+++ GiftSkill/src/handler/InvocationHandler.java
@@ -1,5 +1,26 @@
 package handler;
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+//import com.amazon.ask.model.LaunchRequest;
+import com.amazon.ask.model.Response;
+import com.amazon.ask.request.Predicates;
 
-public class InvocationHandler {
+import java.util.Optional;
 
+public class InvocationHandler implements RequestHandler {
+
+    @Override
+    public boolean canHandle(HandlerInput input) {
+        return input.matches(Predicates.intentName("GiftIntent"));
+    }
+    
+    @Override
+    public Optional<Response> handle(HandlerInput input) {
+        String speechText = "Welcome to the Alexa Skills Kit, you can say hello";
+        return input.getResponseBuilder()
+                .withSpeech(speechText)
+                .withSimpleCard("HelloWorld", speechText)
+                .withReprompt(speechText)
+                .build();
+    }
 }
